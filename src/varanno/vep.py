4f1e980bbf223e2fc77293a103c35e659d80f0e8
alyssflynn <alyss.flynn@me.com>
2024-10-17 11:06:05 -0700

    switch to VEP hgvs bulk endpoint and grch37 API

--- src/varanno/vep.py
+++ src/varanno/vep.py
@@ -1,9 +1,9 @@
 import requests
-from .allele import variant_type
 
 HGVSString = str
 
-VEP_API_BASE_URL = "https://rest.ensembl.org"
+# VEP_API_BASE_URL_LATEST = "https://rest.ensembl.org"
+VEP_API_BASE_URL_GRCh37 = "https://grch37.rest.ensembl.org"
 
 
 def hgvs_string(chromosome: str, pos: str | int, ref: str, alt: str) -> HGVSString:
@@ -15,12 +15,12 @@ def hgvs_string(chromosome: str, pos: str | int, ref: str, alt: str) -> HGVSStri
 
 def first_element(data) -> dict:
     """Returns the first element in a list, otherwise returns the unchanged object."""
-    if type(data) is list:
+    if type(data) is list and data:
         return data[0]
     return data
 
 
-def vep_api_hgvs_get(hgvs_string: HGVSString) -> list[dict]:
+def vep_api_hgvs_get(hgvs_string: HGVSString, species: str = "human") -> list[dict]:
     """Fetch variant consequences from VEP API based on a HGVS notation.
 
     Endpoint: `GET vep/:species/hgvs/:hgvs_notation`
@@ -29,9 +29,26 @@ def vep_api_hgvs_get(hgvs_string: HGVSString) -> list[dict]:
 
     ([VEP API docs](https://rest.ensembl.org/documentation/info/vep_hgvs_get))
     """
-    url = f"{VEP_API_BASE_URL}/vep/human/hgvs/{hgvs_string}?"
+    url = f"{VEP_API_BASE_URL_GRCh37}/vep/{species}/hgvs/{hgvs_string}?"
     
     res = requests.get(url, headers={"Content-Type": "application/json"})
+    if not res.ok:
+        res.raise_for_status()  
+    
+    return first_element(res.json())
+
+
+def batch_vep_hgvs(hgvs_strings: list[HGVSString], species: str = "human") -> list[dict]:
+    """Fetch variant consequences for multiple HGVS notations.
+
+    Endpoint: `POST vep/:species/hgvs`
+    ([VEP API docs](https://grch37.rest.ensembl.org/documentation/info/vep_hgvs_post))
+    """
+    url = f"{VEP_API_BASE_URL_GRCh37}/vep/{species}/hgvs"
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    data = {"hgvs_notations": hgvs_strings}
+    
+    res = requests.post(url, headers=headers, json=data)
     if not res.ok:
         res.raise_for_status()  
     
@@ -48,10 +65,20 @@ def find_vep_gene_id(data: dict) -> str | None:
             return gene_id
     
 
+def find_vep_allele_string(data: dict) -> float:
+    """Retrieves "allele_string" from the VEP API data."""
+    return data.get("allele_string")
+
+
+def find_vep_variant_effect(data: dict) -> float:
+    """Retrieves "most_severe_consequence" from the VEP API data."""
+    return data.get("most_severe_consequence")
+
+
 def find_vep_maf(data: dict, alt: str) -> float:
     """
     Finds the minor allele frequency within the VEP API response. 
-    Located "colocated_variants[0].frequences.{alt}.af".
+    Located at "colocated_variants[0].frequences.{alt}.af".
     """
     maf = None
     if (colocated_variant := first_element(data.get("colocated_variants", []))):
@@ -59,35 +86,3 @@ def find_vep_maf(data: dict, alt: str) -> float:
             maf = frequencies.get(alt, {}).get("af") 
 
     return maf
-
-
-def filter_vep_result(api_output: dict, alt: str):
-    """
-    Using the VEP hgvs API output, gets the gene of the variant, type of variation (substitution,
-    insertion, CNV, etc.) and their effect (missense, silent, intergenic, etc.).
-
-    API docs: https://rest.ensembl.org/documentation/info/vep_region_get
-    """
-    data = first_element(api_output)
-            
-    # get gene of variant
-    gene_id = find_vep_gene_id(data)
-
-    # type of variation
-    allele_string = data.get("allele_string")
-    vtype = variant_type(allele_string) 
-
-    # Variant effect
-    effect = data.get("most_severe_consequence")
-
-    # Minor allele frequency
-    maf = find_vep_maf(data, alt)
-
-    return {
-        "gene_id": gene_id,
-        "allele_string": allele_string,
-        "variant_type": vtype,
-        "variant_effect": effect,
-        "minor_allele_frequency": maf
-    }
-
