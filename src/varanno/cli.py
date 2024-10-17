6949ab04dcdae2a95709b1672b2bb6dafee14105
alyssflynn <alyss.flynn@me.com>
2024-10-17 11:12:50 -0700

    add cli

---
+++ src/varanno/cli.py
@@ -0,0 +1,29 @@
+import argparse
+from .varanno import VCFProcesser
+
+
+def parse_args():
+    """Parse args when provided via command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", 
+        "--file", 
+        required=True, 
+        dest="infile",
+        help="Input VCF file to process."
+    )
+    parser.add_argument(
+        "-o", 
+        "--out", 
+        required=False, 
+        dest="outdest",
+        help="Output destination for annotation results", 
+        default="varanno_output"
+    )
+    return parser.parse_args()
+
+
+def run_annotation():
+    args = parse_args()
+    VCFProcesser(args.infile, args.outdest).process()
+ 
