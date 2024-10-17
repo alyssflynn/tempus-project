1b82628bdfd8b3709faa66645df5737e28d998be
alyssflynn <alyss.flynn@me.com>
2024-10-17 11:12:33 -0700

    refactor functions into main processor class

--- src/varanno/varanno.py
+++ src/varanno/varanno.py
@@ -1,90 +1,77 @@
-from functools import cached_property
+import csv
+import os
+import sys
+import logging
+from dataclasses import asdict
+from .fileio import write_metadata_json, write_logs
+from .record import VariantAnnotation
 from .vcf import Reader, Record
-from .utils import cast_float, parse_record_info, parse_format_sample, parse_genotype
-from .vep import hgvs_string, filter_vep_result, vep_api_hgvs_get
 
 
 __all__ = [
-    "AnnotatedRecord"
+    "VCFProcessor",
+    "Reader",
+    "Record"
 ]
 
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+handlers = [stdout_handler]
 
-class AnnotatedRecord:
-    """
-    Generates the following pieces of information for each variant:
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    handlers=handlers
+)
 
-    1. Depth of sequence coverage at the site of variation.
-    2. Number of reads supporting the variant.
-    3. Percentage of reads supporting the variant versus those supporting reference reads.
-    4. Using the VEP hgvs API, get the gene of the variant, type of variation (substitution,
-       insertion, CNV, etc.) and their effect (missense, silent, intergenic, etc.). The API
-       documentation is available here: https://rest.ensembl.org/#VEP
-    5. The minor allele frequency of the variant if available.
-    6. Any additional annotations that you feel might be relevant.
-    """
-    def __init__(self, record: Record):
-        self.record = record
-        self.info = parse_record_info(self.record.INFO)
-        self.sample = parse_format_sample(self.record.FORMAT, self.record.SAMPLE)
-        self.genotype = parse_genotype(self.info.get("GT"))
-        self.hgvs = hgvs_string(self.record.CHROM, self.record.POS, self.record.REF, self.record.ALT)
-    
-    @cached_property
-    def _vep_api_response(self):
-        return vep_api_hgvs_get(self.hgvs)
-    
-    @cached_property
-    def vep_data(self):
-        return filter_vep_result(self._vep_api_response, self.record.ALT)  
+log = logging.getLogger(__name__)
 
-    @cached_property
-    def total_coverage(self):
-        """
-        Depth of sequence coverage at the site of variation.
 
-        Relevant metadata:
-        ##INFO=<ID=TC..."Total coverage at this locus">
-        ##INFO=<ID=TR..."Total number of reads containing this variant">
-        ##FORMAT=<ID=NR..."Number of reads covering variant location in this sample">
-
-        TODO: verify this is correct
-        """
-        return cast_float(self.info.get("TC"))
-
-    @cached_property
-    def n_reads_supporting_variant(self):
-        """
-        Number of reads supporting the variant.
-
-        Relevant metadata:
-        `##FORMAT=<ID=NV..."Number of reads containing variant in this sample">`
-        `##INFO=<ID=TR..."Total number of reads containing this variant">`
-
-        TODO: verify this is correct
-        """
-        return cast_float(self.sample.get("NV"))
-
-    @cached_property
-    def pct_reads_supporting_variant(self):
-        """
-        Percentage of reads supporting the variant versus those supporting reference reads.
-        """
-        n_var_reads = self.n_reads_supporting_variant
-        total_reads_at_position = self.total_coverage
-
-        try:
-            n_supporting = float(n_var_reads)
-            tot_reads = float(total_reads_at_position)
-            return round((n_supporting / tot_reads) * 100, 4)
+class VCFProcessor:
+    def __init__(self, infile: str, outdir: str, allow_overrides: bool = True):
+        self.infile = infile
+        self.outdir = outdir
+        self.allow_overrides = allow_overrides
         
-        except (ValueError, ZeroDivisionError):
-            return None
+        # Ensure output dir exists, override if allowed
+        os.makedirs(self.outdir, exist_ok=self.allow_overrides)
 
-    @cached_property
-    def annotations(self):
-        return { 
-            **self.vep_data,
-            "depth_of_sequence_coverage": self.total_coverage,
-            "n_reads_supporting_variant": self.n_reads_supporting_variant,
-            "pct_reads_supporting_variant": self.pct_reads_supporting_variant,
-        }
+        # Set output file paths
+        self.annotation_file = os.path.join(self.outdir, 'annotations.csv')
+        self.metadata_file = os.path.join(self.outdir, 'metadata.json')
+        self.error_file = os.path.join(self.outdir, 'errors.log')
+        self.log_file = os.path.join(self.outdir, 'tmp.log')
+        log.addHandler(logging.FileHandler(filename=self.log_file))
+
+    def process(self):
+        self.validate_input_file()
+
+        log.info(f"Annotating VCF! {self.infile} -> {self.outdir}")
+        self.reader = Reader(self.infile)
+
+        # Generate variant annotations and write to file
+        annotation_gen = self.reader.annotation_generator()
+        self.write_record_annotations(annotation_gen, self.annotation_file)
+
+        # Write metadata to file
+        log.info(f"Writing metadata JSON -> {self.metadata_file}")
+        write_metadata_json(self.reader.metadata, self.metadata_file)
+
+        # Write errors to file
+        if self.reader.errors:
+            log.warning(f"Logging {len(self.reader.errors)} errors -> {self.error_file}")
+            write_logs([err.logstr() for err in self.reader.errors], self.error_file)
+        
+    def validate_input_file(self):
+        if not os.path.exists(self.infile):
+            raise FileExistsError("Input file not found")
+        
+    def write_record_annotations(self, annotation_gen):
+        log.info(f"Generating record annotations -> {self.annotation_file}")
+
+        with open(self.annotation_file, "wt") as fle:
+            writer = csv.DictWriter(fle, fieldnames=VariantAnnotation.__slots__)
+            writer.writeheader()
+
+            for variant in map(asdict, annotation_gen):
+                writer.writerow(variant)
+
