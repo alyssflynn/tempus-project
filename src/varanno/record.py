0cde5e6e842d2ff0f4b7e5eda55f18fb5e15db93
alyssflynn <alyss.flynn@me.com>
2024-10-18 22:37:14 -0700

    fix typehints

--- src/varanno/record.py
+++ src/varanno/record.py
@@ -1,35 +1,47 @@
-from dataclasses import dataclass
+import logging
+from dataclasses import dataclass, field
+from typing import Any
 from .allele import variant_type, parse_genotype
 from .utils import cast_float
 from .parse import parse_record_info, parse_format_sample
 from .vep import (
-    batch_vep_hgvs, hgvs_string, vep_api_hgvs_get,
-    find_vep_gene_id, find_vep_maf, find_vep_allele_string, 
-    find_vep_variant_effect
+    HGVSString,
+    batch_vep_hgvs,
+    hgvs_string,
+    vep_api_hgvs_get,
+    find_vep_gene_id,
+    find_vep_maf,
+    find_vep_allele_string,
+    find_vep_variant_effect,
 )
+
+
+log = logging.getLogger(__name__)
 
 
 @dataclass(slots=True)
 class Record:
     CHROM: str
-    POS: int
-    ID: str 
+    POS: str | int
+    ID: str
     REF: str
     ALT: str
     QUAL: str
     FILTER: str
     INFO: str
-    FORMAT: str = None
-    SAMPLE: str = None
+    FORMAT: str | None = None
+    SAMPLE: str | None = None
 
-    line_no: int = None
-    hgvs: str = None
+    line_no: int | None = None
+    hgvs: HGVSString = field(init=False)
 
     def __post_init__(self):
         self.hgvs = hgvs_string(self.CHROM, self.POS, self.REF, self.ALT)
-        
 
-def pct_reads_supporting_variant(n_reads_supporting_variant: float, total_coverage: float):
+
+def pct_reads_supporting_variant(
+    n_reads_supporting_variant: float | Any, total_coverage: float | Any
+):
     """
     Calculates percentage of reads supporting the variant versus those supporting reference reads.
     """
@@ -37,42 +49,53 @@ def pct_reads_supporting_variant(n_reads_supporting_variant: float, total_covera
         var_reads = float(n_reads_supporting_variant)
         tot_reads = float(total_coverage)
         return round((var_reads / tot_reads) * 100, 4)
-    
+
     except (ValueError, ZeroDivisionError):
         return None
-    
+
 
 @dataclass(slots=True)
 class VariantAnnotation:
     CHROM: str
-    POS: int
-    ID: str 
+    POS: str | int
+    ID: str
     REF: str
     ALT: str
     hgvs: str
-    gene_id: str
-    allele_string: str
-    variant_type: str
-    variant_effect: str
-    minor_allele_frequency: float
-    depth_of_sequence_coverage: float
-    num_reads_supporting_variant: float
-    pct_reads_supporting_variant: float
-    genotype: str
-    
-    
-def annotation_factory(record: Record, vep_data: dict = None):
-    """Generate annotations for a given variant record. 
-    """
+    gene_id: str | None
+    allele_string: str | None
+    variant_type: str | None
+    variant_effect: str | None
+    minor_allele_frequency: float | None
+    depth_of_sequence_coverage: float | None
+    num_reads_supporting_variant: float | None
+    pct_reads_supporting_variant: float | None
+    genotype: str | None
+
+
+def annotation_factory(record: Record, vep_data: dict | None = None):
+    """Generate annotations for a given variant record."""
+    # log.info(f"Annotating record: {record}")
+
     info = parse_record_info(record.INFO)
-    sample = parse_format_sample(record.FORMAT, record.SAMPLE)
-    gt = parse_genotype(sample.get("GT"))
+    sample = (
+        parse_format_sample(record.FORMAT, record.SAMPLE)
+        if record.FORMAT and record.SAMPLE
+        else {}
+    )
+    rec_gt = sample.get("GT")
+    genotype = parse_genotype(rec_gt) if rec_gt else None
+    if genotype is None:
+        log.warning(f"Failed to parse genotype for record (pos: {record.POS}, GT:{rec_gt}), setting to 'unknown'")
+        genotype = "unknown"
 
     # Depth of sequence coverage at the site of variation.
-    total_coverage = cast_float(info.get("TC"))
+    rec_tc = info.get("TC")
+    total_coverage = cast_float(rec_tc) if rec_tc else None
 
     # Number of reads supporting the variant.
-    num_var_reads = cast_float(sample.get("NV"))
+    rec_nv = sample.get("NV")
+    num_var_reads = cast_float(rec_nv) if rec_nv else None
 
     # Percentage of reads supporting the variant versus those supporting reference reads.
     pct_supporting = pct_reads_supporting_variant(num_var_reads, total_coverage)
@@ -86,7 +109,7 @@ def annotation_factory(record: Record, vep_data: dict = None):
 
     # type of variation
     allele_string = find_vep_allele_string(vep_data)
-    vtype = variant_type(allele_string) 
+    vtype = variant_type(allele_string) if allele_string else None
 
     # Variant effect
     effect = find_vep_variant_effect(vep_data)
@@ -109,7 +132,7 @@ def annotation_factory(record: Record, vep_data: dict = None):
         depth_of_sequence_coverage=total_coverage,
         num_reads_supporting_variant=num_var_reads,
         pct_reads_supporting_variant=pct_supporting,
-        genotype=gt
+        genotype=genotype,
     )
 
 
