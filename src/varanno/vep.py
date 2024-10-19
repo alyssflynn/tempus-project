d83804bf0263eacf1c3a4e5c8295a1c3234b30b2
alyssflynn <alyss.flynn@me.com>
2024-10-18 22:41:23 -0700

    fix bug dropping records due to VEP response failures

--- src/varanno/vep.py
+++ src/varanno/vep.py
@@ -1,5 +1,9 @@
-import requests
+import logging
 import pydash
+import requests
+
+
+log = logging.getLogger(__name__)
 
 HGVSString = str
 
@@ -11,6 +15,12 @@ def hgvs_string(chromosome: str, pos: str | int, ref: str, alt: str) -> HGVSStri
     """Builds variant string in HGVS notation."""
     if not all((chromosome, pos, ref, alt)):
         raise RuntimeError
+    
+    # TODO: verify this is the correct format
+    if len(ref) > 1: # or len(alt) > 1:
+        posend = int(pos) + len(ref)
+        return f"1:g.{pos}_{posend}{ref}delins{alt}"
+    
     return f"{chromosome}:g.{pos}{ref}>{alt}"
 
 
@@ -36,7 +46,43 @@ def vep_api_hgvs_get(hgvs_string: HGVSString, species: str = "human") -> dict:
     if not res.ok:
         res.raise_for_status()
 
-    return first_element(res.json())
+    data = res.json()
+
+    if type(data) is dict and (error := data.get("error")):
+        log.warning(error)
+
+    return first_element(data)
+
+
+def hgvs_response_input_dict(hgvs_response: list[dict]) -> dict[str, dict]:
+    """Returns a dictionary of VEP hgvs responses keyed on the "input" param."""
+
+    def _gen_dict(responses):
+        for res in responses:
+            yield res.get("input"), res
+
+    return dict(_gen_dict(hgvs_response))
+
+
+def realign_hgvs_inputs_outputs(
+    hgvs_response: list[dict], hgvs_strings: list[HGVSString]
+):
+    """
+    Yields VEP hgvs API results in the order the "hgvs_notations" input list was provided,
+    mapping dummy error outputs to the keys which are missing outputs in the response.
+
+    This ensures API inputs and outputs are aligned/of the same length, and is necessary
+    because he VEP hgvs API bulk endpoint appears to ignore malformed inputs rather than
+    providing any explicit error messaging.
+    """
+    results = {res.get("input"): res for res in hgvs_response}
+    for hgvs in hgvs_strings:
+        res = results.get(hgvs)
+        if res:
+            yield res
+        else:
+            log.warning(f"VEP API returned no data for HGVS: {hgvs}")
+            yield {"error": "no data returned"}
 
 
 def batch_vep_hgvs(
@@ -55,7 +101,15 @@ def batch_vep_hgvs(
     if not res.ok:
         res.raise_for_status()
 
-    return res.json()
+    results = res.json()
+
+    # NOTE: The VEP hgvs API bulk endpoint ignores inputs it doesn't like rather
+    # than including errors, so the inputs and outputs may not be the same length.
+    # In these cases we need to realign them and handle the missing data.
+    if len(hgvs_strings) != len(results):
+        results = list(realign_hgvs_inputs_outputs(results, hgvs_strings))
+
+    return results
 
 
 def find_vep_gene_id(data: dict) -> str | None:
